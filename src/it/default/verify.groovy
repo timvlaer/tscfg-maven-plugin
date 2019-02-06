e20d945b6bd2dd8cb3417e6d949165f78f66c4d1
Tim Van Laer <tim.vanlaer@sentiance.com>
2019-02-06 10:28:19 +0100

    Check contents of generated file in integration test (spot check)

--- src/it/default/verify.groovy
+++ src/it/default/verify.groovy
@@ -1,23 +1,13 @@
-File file = new File((File)basedir, "target/generated-sources/tscfg/com/github/timvlaer/generated/config/TestConfig.java")
-if (!file.isFile()) {
-    throw new FileNotFoundException("Generated class not found: " + file)
-}
+import static org.assertj.core.api.Assertions.assertThat
+import static org.assertj.core.api.Assertions.contentOf
 
-/*
-TODO tim, try to validate the generated class:
-File classpath = new File("target/its/default/target/classes");
-ClassLoader cl = new URLClassLoader([classpath.toURI().toURL()] as URL[]);
+def generatedClass = new File((File) basedir, "target/generated-sources/tscfg/com/github/timvlaer/generated/config/TestConfig.java")
+assertThat(generatedClass).exists()
 
-Class cls = cl.loadClass("com.github.timvlaer.generated.config.TestConfig");
-Constructor constructor = cls.getConstructors()[0];
-Object config = constructor.newInstance(com.typesafe.config.ConfigFactory.load("tscfg/test.conf"))
-assertThat(config).isNotNull()
+assertThat(contentOf(generatedClass))
+        .contains("String getProperty1()")
+        .contains("Optional<java.lang.String> getProperty2()")
+        .contains("int getProperty3()")
+        .contains("Duration getProperty4()")
 
-Method getTestMethod = config.getClass().getMethod("getTest");
-Object testObject = getTestMethod.invoke(config);
-assertThat(testObject).isNotNull();
-
-assertThat(invoke(testObject, "getProperty1")).isEqualTo("a");
-assertThat(invoke(testObject, "getProperty2")).isEqualTo(java.util.Optional.empty());
-assertThat(invoke(testObject, "getProperty3")).isEqualTo(3);
-assertThat(invoke(testObject, "getProperty4")).isEqualTo(Duration.of(10, ChronoUnit.MINUTES));*/
+return true
